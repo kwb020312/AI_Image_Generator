332e8494d0f1bc6273b0f374c673783d93dabb34
N S <kwb020312@naver.com>
2023-04-07 20:55:45 +0900

    백엔드 서버 배포 URL 반영

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,12 +22,15 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://ai-image-generator-backend-six.vercel.app/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
